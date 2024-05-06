d26e95a53dba59c3ba6464cfab1dc85f992ca1e1
Souvlaki42 <souvlaki420@gmail.com>
2024-05-06 12:38:47 +0300

    Pass 5th stage

--- app/server.go
+++ app/server.go
@@ -2,9 +2,29 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net"
 	"os"
 )
+
+func handleConnection(c net.Conn) {
+	defer c.Close()
+	for {
+		b := make([]byte, 1024)
+		n, err := c.Read(b)
+		if err != nil {
+			if err != io.EOF {
+				fmt.Println("Error reading commands: ", err.Error())
+			}
+			continue
+		}
+		fmt.Printf("Received %d bytes: %q\n", n, b[:n])
+		_, err = c.Write([]byte("+PONG\r\n"))
+		if err != nil {
+			fmt.Println("Error writing output: ", err.Error())
+		}
+	}
+}
 
 func main() {
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
@@ -12,23 +32,13 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	c, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
-	}
+	defer l.Close()
 	for {
-		b := make([]byte, 1024)
-		_, err := c.Read(b)
-		fmt.Println(string(b))
+		c, err := l.Accept()
 		if err != nil {
-			fmt.Println("Error reading commands: ", err.Error())
-			c.Close()
-		}
-		_, err = c.Write([]byte("+PONG\r\n"))
-		if err != nil {
-			fmt.Println("Error responding to commands: ", err.Error())
+			fmt.Println("Error accepting connection: ", err.Error())
 			os.Exit(1)
 		}
+		go handleConnection(c)
 	}
 }
